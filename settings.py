1028429a584aabbb2d26804f810497cb0c9fe572
Rory McStay <rory@rorymcstay.com>
2020-01-11 14:30:07 +0000

    platfoorm readme

--- settings.py
+++ settings.py
@@ -1,5 +1,10 @@
 import os
 
+subscribe_params = {
+    "topics": os.getenv("TOPIC_TYPE", "results")
+}
+
+
 table_params = {
     "prefix": os.getenv("TABLE_PREFIX", "stg"),
     "type": os.getenv("TABLE_TYPE", "results"),
