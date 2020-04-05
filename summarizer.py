4be17faa1fffb7428a786b1de011dd9c648ac325
Rory McStay <rory@rorymcstay.com>
2020-04-05 15:47:48 +0100

    summarizer cli changes

--- summarizer.py
+++ summarizer.py
@@ -3,7 +3,7 @@ import os
 import argparse
 from src.main.loader import ResultLoader
 
-logging = getLogger('summarizer.log')
+logging = getLogger(__name__)
 
 logging.info(f'starting summarizer')
 
@@ -11,11 +11,21 @@ argparser = argparse.ArgumentParser("Transform data from kafak feed to flatten s
 
 argparser.add_argument('--produceObjects', action='store_true', default=False)
 
+argparser.add_argument('--headlessMode', action='store_true', default=False)
+
 if __name__ == "__main__":
     rl = ResultLoader()
     args = argparser.parse_args()
     if args.produceObjects:
-
         logging.info(f'starting producing objects')
         rl.produceObjects()
         logging.info(f'Goodbye...')
+    elif args.headlessMode:
+        while True:
+            rl.produceObjects()
+            if rl.lastCollected == 0:
+                logging.info(f'no objects to collect, will wait 60s')
+                sleep(60)
+                continue
+
+
