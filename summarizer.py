321b36a21621f7eaac4ffeebfeb73d06386cacb4
Rory McStay <rory@rorymcstay.com>
2020-05-26 18:48:55 +0100

    common logging settings

--- summarizer.py
+++ summarizer.py
@@ -3,7 +3,7 @@ import logging
 import os
 from src.main.parser import ResultParser
 from feed.actionchains import KafkaActionSubscription
-from feed.settings import nanny_params
+from feed.settings import nanny_params, logger_settings_dict
 from logging.config import dictConfig
 
 
@@ -14,21 +14,7 @@ class CaptureActionRunner(KafkaActionSubscription):
         KafkaActionSubscription.__init__(self, topic=queue, implementation=ResultParser)
 
 if __name__ == '__main__':
-    dictConfig({
-        'version': 1,
-        'formatters': {'default': {
-            'format': '[%(asctime)s]%(thread)d: %(module)s - %(levelname)s - %(message)s',
-        }},
-        'handlers': {'wsgi': {
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://flask.logging.wsgi_errors_stream',
-            'formatter': 'default'
-        }},
-        'root': {
-            'level': 'INFO',
-            'handlers': ['wsgi']
-        }
-    })
+    dictConfig(logger_settings_dict)
 
     logging.getLogger("urllib3").setLevel("INFO")
 
